441776865c15bf3f7b2f7a3dc8b5b7bd596b66ec
Enrico <enricoore@gmail.com>
2022-05-29 13:00:26 +0700

    Initial Commit

---
+++ mojimoku/mojimokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  mojimokuApp.swift
+//  mojimoku
+//
+//  Created by MacBook Pro on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct mojimokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
